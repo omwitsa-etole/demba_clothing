5bb38817aa332da3a7dfa2dd49091e711ae22079
omwitsa-etole <omwitsaetole@gmail.com>
2025-05-03 13:49:34 +0300

    add req

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 app.secret_key = 'ussd'
 #app. = True
 #app.secret_key = 'ussd'
-API_URL = "https://8eac-154-154-236-113.ngrok-free.app"#"http://localhost:64634"
+API_URL = "https://e6f4-102-2-132-28.ngrok-free.app"#"http://localhost:64634"
 
 ALL_FEED = []
 ALL_PRODUCT = []
